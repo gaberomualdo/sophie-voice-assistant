172e7b3f1b70141037b56c8644d86f52552afe3b
xtrp <36894883+xtrp@users.noreply.github.com>
2020-03-06 23:24:48 +0000

    record audio with microphone, send to IBM cloud for speech to text, done

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,4 +1,10 @@
-// event fire function, some code taken from user Kooilnc on https://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
+// ipc renderer module for communicating directly with the main process
+const { ipcRenderer } = require('electron');
+const fs = require('electron').remote.require('fs');
+const uuid = require('electron').remote.require('uuid');
+
+// event fire function
+// some code taken from user Kooilnc on https://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
 function fireDOMEvent(el, etype){
     if (el.fireEvent) {
         el.fireEvent('on' + etype);
@@ -9,10 +15,104 @@ function fireDOMEvent(el, etype){
     }
 }
 
+// elm variables
+const microphoneBtnElm = document.querySelector('.center_audio_box .listen_for_audio');
+
 // press space to click microphone btn
-document.addEventListener("keydown", (e) => {
+document.addEventListener('keydown', (e) => {
     if(e.keyCode == 32) {
         // space bar was hit, simulation microphone btn click
-        fireDOMEvent(document.querySelector(".center_audio_box .listen_for_audio"), "click");
+        fireDOMEvent(microphoneBtnElm, 'click');
+    }
+});
+
+// function to record audio
+// code from most of this function taken from Bryan Jennings on https://medium.com/@bryanjenningz/how-to-record-and-play-audio-in-javascript-faa1b2b3e49b
+const recordAudioWithMicrophone = () => {
+    return new Promise(resolve => {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            const mediaRecorder = new MediaRecorder(stream);
+            const audioChunks = [];
+
+            mediaRecorder.addEventListener('dataavailable', event => {
+                audioChunks.push(event.data);
+            });
+
+            const start = () => {
+                mediaRecorder.start();
+            };
+
+            const stop = () => {
+                return new Promise(resolve => {
+                    mediaRecorder.addEventListener('stop', () => {
+                    const audioBlob = new Blob(audioChunks);
+                    const audioURL = URL.createObjectURL(audioBlob);
+                    const audio = new Audio(audioURL);
+                    const play = () => {
+                        audio.play();
+                    };
+
+                    resolve({ audioBlob, audioURL, play });
+                });
+
+                mediaRecorder.stop();
+                });
+            };
+
+            resolve({ start, stop });
+        });
+    });
+};
+
+// variable for to be defined audio recorder (used on click of microphone btn elm)
+let audioRecorder = null;
+
+// onclick event for record audio button
+microphoneBtnElm.addEventListener('click', async (e) => {
+    microphoneBtnElm.blur();
+    
+    if(!navigator.onLine) {
+        alert("No Internet Connection");
+        return;
+    }
+
+    if(!microphoneBtnElm.classList.contains('listening')) {
+        // microphone is listening, start recording audio
+        audioRecorder = await recordAudioWithMicrophone();
+        audioRecorder.start();
+
+        setTimeout(() => {
+            microphoneBtnElm.classList.toggle('listening');
+        }, 250);
+    }else {
+        microphoneBtnElm.classList.toggle('listening');
+
+        // microphone ended listening, end recording and parse
+        
+        // recorded audio obj
+        const recordedAudio = await audioRecorder.stop();
+        
+        // play audio (for testing purposes)
+        // recordedAudio.play();
+
+        // recorded audio filename (for use later)
+        const recordedAudioFilename = uuid.v4() + '.wav';
+
+        // audio blob
+        let audioBlob = recordedAudio.audioBlob;
+        audioBlob.lastModifiedDate = new Date();
+        audioBlob.name = recordedAudioFilename;
+        
+        // read recorded audio into wav file
+        const fileReaderObj = new FileReader();
+        fileReaderObj.onload = function() {
+            const recordedAudioFilePath = './raw_audio/inputted/' + recordedAudioFilename;
+            fs.writeFileSync(recordedAudioFilePath, Buffer.from(new Uint8Array(this.result)));
+            
+            // send audio file path to main process app module for analysis
+            ipcRenderer.send('interpret-audio', recordedAudioFilePath);
+        };
+        fileReaderObj.readAsArrayBuffer(audioBlob);
     }
 });
--- main.js
+++ main.js
@@ -5,10 +5,10 @@
 */
 
 // get required modules from electron
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 // get Node.js modules used in app
-const fs = require("fs");
+const fs = require('fs');
 
 // global window object
 let window;
@@ -31,7 +31,7 @@ function createWindow () {
     };
 
     // macOS has an inset titlebar
-    if(process.platform === "darwin") {
+    if(process.platform === 'darwin') {
         windowOptions.titleBarStyle = 'hiddenInset';
     }
 
@@ -39,37 +39,26 @@ function createWindow () {
     window = new BrowserWindow(windowOptions);
 
     // load main HTML file into window with platform info
-    window.loadFile("index.html");
-
-    // set variable for onblur and onfocus
-    let windowIsFocused = false;
-    window.on("focus", () => {
-        windowIsFocused = true;
-    });
-    window.on("blur", () => {
-        windowIsFocused = false;
-    });
-
-    // send focused info as reply to ipc message from renderer process
-    ipcMain.on("is-window-focused", (event) => {
-        event.returnValue = windowIsFocused;
-    });
+    window.loadFile('index.html');
 }
 
 // create window
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
 // quit when all windows are closed
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
     // on macOS apps stay open until quit in menu-bar or w/Cmd+Q
-    if (process.platform !== "darwin"){
+    if (process.platform !== 'darwin'){
         app.quit();
     }
 });
 
 // on macOS re-create app window when app icon is clicked
-app.on("activate", () => {
+app.on('activate', () => {
     if (window === null){
         createWindow();
     }
 });
+
+/* app module for communicating with frontend, APIs, etc. */
+require('./app');
